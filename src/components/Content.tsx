5c882d867da3f6083db5b16298b736a86cf58d0d
Luciano Zang <zangeronimo@gmail.com>
2021-04-03 18:04:40 -0300

    terminado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
-  // Complete aqui
+import React from "react";
+import { GenreResponseProps, MovieProps } from "../App";
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
 }
+
+export const Content: React.FC<ContentProps> = ({ selectedGenre, movies }) => {
+  return (
+    <>
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+};
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import React from "react";
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  handleClickButton: Function;
+  selectedGenreId: number;
 }
+
+export const SideBar: React.FC<SideBarProps> = ({
+  genres,
+  handleClickButton,
+  selectedGenreId,
+}) => {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+};
